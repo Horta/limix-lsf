db955c1e0a0d9870e8074198c0a5bbefe6de2a5e
Danilo Horta <danilo.horta@gmail.com>
2017-02-14 17:21:22 +0000

    export api

--- limix_lsf/__init__.py
+++ limix_lsf/__init__.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import as _absolute_import
 
-from pkg_resources import get_distribution as _get_distribution
 from pkg_resources import DistributionNotFound as _DistributionNotFound
+from pkg_resources import get_distribution as _get_distribution
 
+from . import clusterrun
+from .util import kill_group
 
 try:
     __version__ = _get_distribution('limix_lsf').version
 except _DistributionNotFound:
     __version__ = 'unknown'
+
 
 def test():
     import os
@@ -25,3 +28,6 @@ def test():
         print("Congratulations. All tests have passed!")
 
     return return_code
+
+
+__all__ = ['__version__', 'test', 'clusterrun', 'kill_group']
