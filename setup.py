a11798e9b3a3a4e94cb472e3d6bca418e3f2c5b6
Danilo Horta <danilo.horta@gmail.com>
2017-02-14 17:17:21 +0000

    pickle blosc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from setuptools import setup
-from setuptools import find_packages
+
+from setuptools import find_packages, setup
 
 
 def setup_package():
@@ -14,10 +14,11 @@ def setup_package():
     pytest_runner = ['pytest-runner'] if needs_pytest else []
 
     setup_requires = [] + pytest_runner
-    install_requires = ['pytest', 'scipy>=0.17', 'numpy>=1.9',
-                        'tabulate', 'limix_util>=1.0.4', 'humanfriendly',
-                        'futures', 'tqdm', 'pickle_mixin']
-    tests_require = install_requires
+    install_requires = [
+        'pytest', 'scipy>=0.17', 'numpy>=1.9', 'tabulate', 'humanfriendly',
+        'futures', 'tqdm', 'pickle_mixin', 'pickle_blosc'
+    ]
+    tests_require = ['pytest']
 
     metadata = dict(
         name='limix_lsf',
@@ -25,40 +26,26 @@ def setup_package():
         maintainer="Danilo Horta",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
-        url='http://github.com/Horta/limix-lsf',
+        url='http://github.com/glimix/limix-lsf',
         packages=find_packages(),
         install_requires=install_requires,
         setup_requires=setup_requires,
         tests_require=tests_require,
         include_package_data=True,
         zip_safe=True,
-        entry_points={
-            'console_scripts': ['bj = limix_lsf.bj:entry_point']
-        },
+        entry_points={'console_scripts': ['bj = limix_lsf.bj:entry_point']},
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.4",
-            "Programming Language :: Python :: 3.5",
             "Operating System :: OS Independent",
-        ],
-    )
-
-    try:
-        from distutils.command.bdist_conda import CondaDistribution
-    except ImportError:
-        pass
-    else:
-        metadata['distclass'] = CondaDistribution
-        metadata['conda_buildnum'] = 1
-        metadata['conda_features'] = ['mkl']
+        ], )
 
     try:
         setup(**metadata)
     finally:
         del sys.path[0]
         os.chdir(old_path)
+
 
 if __name__ == '__main__':
     setup_package()
